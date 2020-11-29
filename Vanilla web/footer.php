71f750c166b58085f1ce546f2ac327dadac5ea3a
Edwin Bartlett <teddybartletg88@gmail.com>
2020-11-29 08:03:34 +0800

    removed folder

--- Vanilla web/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="footmain">
-      <h5 id="foot_logo">libera</h5>
-      <a href="#">Info</a>&nbsp•&nbsp
-      <a href="#">Support</a><br>
-      <a href="#">Terms of Use</a>&nbsp•&nbsp<a href="#">Privacy Policy</a>
-      <div class="reserve">@2020 COMP NAME ALL RIGHTS RESERVED</div>
-    </div>
-  </footer>
